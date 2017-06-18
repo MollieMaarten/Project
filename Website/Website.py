211d0a6d3572de8088b1bab3f990545629096b13
Maarten Mollie <maarten.mollie@student.howest.be>
2017-06-18 18:08:33 +0200

    Add files via upload

--- Website/Website.py
+++ Website/Website.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask import render_template
+from DbClass import DbClass
+
+import os
+from os import environ
 
 app = Flask(__name__)
 
@@ -25,4 +29,7 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run()
+    #check omgevingsvariabele voor poort of neem 8080 als standaard
+    port = int(os.environ.get("PORT",8080))
+    host = "169.254.10.11" #luister op alle IP's       i.p.v enkel 127.0.0.1
+    app.run(host=host, port=port, debug=True)
